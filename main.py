0f157cdaac62f621e3d6788d7ed29cc284e96565
陳羿帆 <s444910@sinyi.com.tw>
2022-06-10 13:20:47 +0800

    let attack

--- main.py
+++ main.py
@@ -34,10 +34,37 @@ def move():
     logger.info(request.json)
     my_x = request.json['arena']['state']['https://squirtlefire-mllc3dma5q-uc.a.run.app']['x']
     my_y = request.json['arena']['state']['https://squirtlefire-mllc3dma5q-uc.a.run.app']['y']
+    my_direction = request.json['arena']['state']['https://squirtlefire-mllc3dma5q-uc.a.run.app']['direction']
 
-    logger.info(str(my_x))
-    logger.info(str(my_y))
-    return moves[random.randrange(3)]
+    if my_direction == 'N':
+        for player in request.json['arena']['state']:
+            player_x = player['x']
+            player_y = player['y']
+            if player_x == my_x and player_y - my_y <=3:
+                return  'T'
+
+    if my_direction == 'S':
+        for player in request.json['arena']['state']:
+            player_x = player['x']
+            player_y = player['y']
+            if player_x == my_x and my_y - player_y <=3:
+                return  'T'
+    
+    if my_direction == 'W':
+        for player in request.json['arena']['state']:
+            player_x = player['x']
+            player_y = player['y']
+            if player_y == my_y and my_x - player_x <=3:
+                return  'T'
+    
+    if my_direction == 'E':
+        for player in request.json['arena']['state']:
+            player_x = player['x']
+            player_y = player['y']
+            if player_y == my_y and player_x - my_x <=3:
+                return  'T'
+
+    return random.choice(['F', 'L', 'R'])
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
